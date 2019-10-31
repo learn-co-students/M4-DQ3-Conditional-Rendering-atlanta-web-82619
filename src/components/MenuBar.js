45597f374d546a2366167e4c21222aff21943ad4
Alexandria <alex.pugia816@hotmail.com>
2019-10-31 10:40:20 -0400

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,38 +1,29 @@
 import React from 'react'
 
-const MenuBar = (props) => {
-
-  /*
-
-  The 'a' tags below are the menu items. Think about the way a menu 
-  should work. When you click a menu item, the button typically becomes
-  'active' to indicate that it is currently selected. How could we achieve
-  this programatically? What other behavior do we expect when we click
-  on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
-
-  */
+const MenuBar = ({changeSelected, selected}) => {
+  const itemClicked = (e) => {
+    changeSelected(e.target.id)
+  }
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick={itemClicked} className={"item" + (selected === "profile" ? " active" : "")} id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={itemClicked} className={"item" + (selected === "photo" ? " active" : "")} id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={itemClicked} className={"item" + (selected === "cocktail" ? " active" : "")} id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={itemClicked} className={"item" + (selected === "pokemon" ? " active" : "")} id="pokemon"> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
-
 }
 
 export default MenuBar
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
 import MenuBar from '../components/MenuBar.js'
 import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
-class MainBox extends React.Component {
+class MainBox extends Component {
+  state = {
+    selected: "profile"
+  }
 
+  changeSelected = (selected) => {
+    this.setState({
+      selected: selected
+    })
+  }
 
   render() {
+    let detailsToDisplay
 
-    /*
-
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
-    */
-
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    switch (this.state.selected) {
+      case "profile":
+        detailsToDisplay = <Profile />
+        break;
+      case "photos":
+        detailsToDisplay = <Photos />
+        break;
+      case "cocktails":
+        detailsToDisplay = <Cocktails />
+        break;
+      case "pokemon":
+        detailsToDisplay = <Pokemon />
+        break;
+      default:
+        detailsToDisplay = null
+        break;
+    }
 
     return (
       <div>
-        <MenuBar />
+        <MenuBar selected={this.state.selected} changeSelected={this.changeSelected}/>
         {detailsToDisplay}
       </div>
     )
   }
-
 }
 
 export default MainBox
